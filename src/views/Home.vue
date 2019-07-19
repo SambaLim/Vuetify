90a3c4aefaafa8777c450a308f1b62c441d9c853
SambaLim <limsungho07@hanmail.net>
2019-07-19 14:23:02 +0900

    Grid System 2

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,29 +3,27 @@
     <h1 class="subheading grey--text">Home</h1>
 
     <v-container class="my-5">
-      <v-layout row wrap>
-        <v-flex xs12 md6>
-          <v-btn outline block class="primary">1</v-btn>
-        </v-flex>
-        <v-flex xs4 md2>
-          <v-btn outline block class="primary">2</v-btn>
-        </v-flex>
-        <v-flex xs4 md2>
-          <v-btn outline block class="primary">3</v-btn>
-        </v-flex>
-        <v-flex xs4 md2>
-          <v-btn outline block class="primary">4</v-btn>
-        </v-flex>
-      </v-layout>
-
-      <v-layout row wrap justify-space-around>
-        <v-flex xs4 md3>
-          <v-btn outline block class="success">1</v-btn>
-        </v-flex>
-        <v-flex xs4 md3>
-          <v-btn outline block class="success">2</v-btn>
-        </v-flex>
-      </v-layout>
+      <v-card flat class="pa-3" v-for="i in 3" :key="i">
+        <v-layout row wrap>
+          <!-- title-->
+          <v-flex xs12 md6>
+              <div class="caption grey--text">Project title</div>
+              <div>Create new Website</div>
+          </v-flex>
+          <v-flex xs6 sm4 md2>
+            <div class="caption grey--text">Person</div>
+            <div>SambaLim</div>
+          </v-flex>
+          <v-flex xs6 sm4 md2>
+            <div class="caption grey--text">Due by</div>
+            <div>19th July 2019</div>
+          </v-flex>
+          <v-flex xs6 sm4 md2>
+            <div class="caption grey--text">Status</div>
+            <div>On Going</div>
+          </v-flex>
+        </v-layout>
+      </v-card>
     </v-container>
   </div>
 </template>
