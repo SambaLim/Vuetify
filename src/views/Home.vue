ff0c15d1a7943c85314a307fb164ecdc7d113833
SambaLim <limsungho07@hanmail.net>
2019-07-19 09:51:58 +0900

    color and text control

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,18 @@
 <template>
   <div class="home">
     <h1>HomePage</h1>
+    <p class="red blue--text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic ex ea laudantium impedit eos officia! Dolor iure sint, tempore ex ipsa sequi magni, at ut voluptatibus porro labore praesentium unde?</p>
+    <p class="pink lighten-4 red--text text--darken-4">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et nulla, error ab neque quos adipisci assumenda accusantium. Rerum id porro in nemo obcaecati, dolore, molestiae maxime eius eaque minima perspiciatis.</p>
+    <h1 class="display-4">Massive Display</h1>
+    <h4 class="display-1">Smaller-display</h4>
+    <p class="headline">This is a headline</p>
+    <p class="subheading font-weight-bold">This is a subheading</p>
+    <p class="caption">This is a caption</p>
   </div>
 </template>
 
 <script>
 export default {
-  
+
 }
 </script>
