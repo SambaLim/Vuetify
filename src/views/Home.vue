96ba9aa3c0308688a8d9ed84fbb95aa2d8d6d5ac
SambaLim <limsungho07@hanmail.net>
2019-07-19 10:03:34 +0900

    buttons and icons control

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,30 @@
 <template>
   <div class="home">
     <h1>HomePage</h1>
-    <p class="red blue--text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic ex ea laudantium impedit eos officia! Dolor iure sint, tempore ex ipsa sequi magni, at ut voluptatibus porro labore praesentium unde?</p>
-    <p class="pink lighten-4 red--text text--darken-4">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et nulla, error ab neque quos adipisci assumenda accusantium. Rerum id porro in nemo obcaecati, dolore, molestiae maxime eius eaque minima perspiciatis.</p>
-    <h1 class="display-4">Massive Display</h1>
-    <h4 class="display-1">Smaller-display</h4>
-    <p class="headline">This is a headline</p>
-    <p class="subheading font-weight-bold">This is a subheading</p>
-    <p class="caption">This is a caption</p>
+
+    <v-btn class="pink white--text">click me</v-btn>
+    <v-btn depressed color="pink">click me</v-btn>
+    <v-btn flat color="pink">click me</v-btn>
+
+    <v-btn depressed class="pink white--text">
+      <v-icon left>email</v-icon>
+      <span>email me</span>
+    </v-btn>
+
+    <v-btn depressed small class="pink white--text">
+      <v-icon left small>email</v-icon>
+      <span>email me</span>
+    </v-btn>
+
+    <v-btn depressed large class="pink white--text">
+      <span>email me</span>
+      <v-icon right large>email</v-icon>
+    </v-btn>
+
+    <v-btn fab small dark color="purple">
+      <v-icon>favorite</v-icon>
+    </v-btn>
+
   </div>
 </template>
 
